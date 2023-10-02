4b060d43a6c9457babe3690301df46ddd2c46a25
alexander <aschmutz@student.tgm.ac.at>
2023-10-02 17:33:38 +0200

    Fixed Exception Types and String comparisons being done with ==

--- src/main/java/aschmutz/Wordpair.java
+++ src/main/java/aschmutz/Wordpair.java
@@ -14,20 +14,20 @@ public class Wordpair {
 		setImageUrl(url);
 	}
 	public void setImageUrl(String url) {
-		if(url == null) throw new RuntimeException("image url must not be null");
+		if(url == null) throw new IllegalArgumentException("image url must not be null");
 		try {
-			if(url.length()==0) throw new RuntimeException("image url must not be empty");
+			if(url.length()==0) throw new IllegalArgumentException("image url must not be empty");
 			new URL(url);
 			} catch (MalformedURLException e) {
-				throw new RuntimeException("image url must not be a valid URL");
+				throw new IllegalArgumentException("image url must not be a valid URL");
 		}
 		this.imageUrl = url;
 	}
 	public String getImageUrl(){return this.imageUrl;}
 
 	public void setWord(String word) {
-		if(word==null) throw new RuntimeException("word must not be null");
-		if(word.length()==0) throw new RuntimeException("image url must not be empty");
+		if(word==null) throw new IllegalArgumentException("word must not be null");
+		if(word.length()==0) throw new IllegalArgumentException("image url must not be empty");
 		this.word = word;
 	}
 
@@ -39,8 +39,8 @@ public class Wordpair {
 	public boolean equals(Object o){
 		if (!(o instanceof Wordpair)) throw new IllegalArgumentException("Object must be comparable to Wordpair");
 		Wordpair wp = (Wordpair) o;
-		if(this.word != wp.word) return false;
-		if(this.imageUrl != wp.imageUrl) return false;
+		if(!this.word.equals(word)) return false;
+		if(!this.imageUrl.equals(wp.imageUrl)) return false;
 		return true;
 	}
 	public String toString(){
