f0615896738e5af479d5bd872d872b2d6ccc6b12
Alexander <aschmutz@student.tgm.ac.at>
2023-10-03 17:52:10 +0200

    Added Documentation to the Interface

--- src/main/java/aschmutz/WordtrainerSaveManager.java
+++ src/main/java/aschmutz/WordtrainerSaveManager.java
@@ -1,21 +1,21 @@
 package aschmutz;
 
 /**
- * TODO
+ * Handels Saving and Loading of WordtrainerBackend.
  */
 public interface WordtrainerSaveManager {
 
     /**
-     * TODO
-     * @param path
-     * @return
+     * Loads a {@link WordtrainerBackend} from a File at a given Path and Returns it
+     * @param path the Path from which the WordtrainerBackend may be loaded
+     * @return The WordtrainerBackend, that is loaded. This loaded WordtrainerBackend is equal to the saved WordtrainerBackend
      */
     public WordtrainerBackend load(String path);
 
     /**
-     * TODO
-     * @param path
-     * @param wordtrainerBackend
+     * Saves a {@link WordtrainerBackend} into a file, at a given path
+     * @param path The path at which the File containing the WordtrainerBackend will be stored
+     * @param wordtrainerBackend the WordtrainerBackend Object that is to be stored.
      */
     public void save(String path, WordtrainerBackend wordtrainerBackend);
 }
